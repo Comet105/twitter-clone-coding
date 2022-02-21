73d4ccb225f5c736a98676767c6409516581562e
Comet105 <taegyeon512@naver.com>
2022-02-21 20:54:51 +0900

    add tweet creation function using firebase/dbService

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import { authService } from "../fbase";
 import {
   createUserWithEmailAndPassword,
+  GithubAuthProvider,
+  GoogleAuthProvider,
   signInWithEmailAndPassword,
+  signInWithPopup,
 } from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -35,8 +40,24 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
+  };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "Google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "Github") {
+      provider = new GithubAuthProvider();
+    }
+    const data = await signInWithPopup(authService, provider);
+    console.log(data);
   };
 
   return (
@@ -59,11 +80,22 @@ const Auth = () => {
           onChange={onChange}
         />
 
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign in"}
+        />
       </form>
+      {error}
+      <h2 onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </h2>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="Google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="Github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "../fbase";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+function Home({ userObj }) {
+  const [tweet, setTweet] = useState("");
+
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(dbService, "tweets"),
+      orderBy("createAt", "desc")
+    );
+
+    onSnapshot(q, (snapshot) => {
+      const tweetArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setTweets(tweetArr);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "tweets"), {
+        text: tweet,
+        createAt: Date.now(),
+        creatorId: userObj.uid,
+      });
+      console.log("Document written with ID:", docRef.id);
+    } catch (error) {
+      console.error("Error adding document:", error);
+    }
+    setTweet("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.text}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Home;
