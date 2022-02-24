562d334825ce65f579e41cb214f9760cec25d704
Comet105 <taegyeon512@naver.com>
2022-02-24 21:23:34 +0900

    Profile edit in progress

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,56 @@
-import React from "react";
-import { authService } from "../fbase";
+import React, { useEffect, useState } from "react";
+import { authService, dbService } from "../fbase";
 import { useNavigate } from "react-router-dom";
+import { updateProfile } from "firebase/auth";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const navigate = useNavigate();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     navigate("/");
   };
-  return <button onClick={onLogOutClick}>Log Out</button>;
+  const getMyTweets = async () => {
+    const q = query(
+      collection(dbService, "tweets"),
+      where("creatorld", "==", userObj.uid)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+  useEffect(() => {
+    getMyTweets();
+  }, []);
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await updateProfile(userObj, { displayName: newDisplayName });
+    }
+  };
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display name"
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 };
 
 export default Profile;
